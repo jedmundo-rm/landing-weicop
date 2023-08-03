01e9e44aecd18c535b93e4504d3175a0abd2bfb0
jedmundo-rm <79552053+jedmundo-rm@users.noreply.github.com>
2023-08-03 17:14:38 -0600

    subida de archivos

---
+++ js/main.js
@@ -0,0 +1,51 @@
+jQuery(function($){
+
+    function headerTexts(button,text,hiddentext) {
+        $(hiddentext).hide();
+        $(button).click(function () {
+            $(text).toggle();
+            $(hiddentext).toggle();
+            $(this).toggleClass("activebutton");
+        });
+    }
+    headerTexts(".btn1","#text1","#hidden1");
+    headerTexts(".btn2","#text2","#hidden2");
+    headerTexts(".btn3","#text3","#hidden3");
+
+
+
+   
+    $("#menu img").click(function(){
+
+
+    });
+
+
+    var action = 1;
+
+    $("#menu img").on("click", viewSomething);
+
+    function viewSomething() {
+        if ( action == 1 ) {
+            $(this).attr("src", "./img/cerrar.svg");
+        action = 2;
+        } else {
+        $(this).attr("src", "./img/menu.svg");
+        action = 1;
+        }
+    }
+
+/////////////////
+$("a#someId").on("click", function(){
+        
+    //Put the code from above here.
+    $.post("modal.html", function(data){
+
+        $("#myModalDiv").html(data).fadeIn();
+    
+    });
+
+});    
+
+
+});
